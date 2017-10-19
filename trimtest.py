78e18986b843115d17b92383858a3aaedc01fdc2
Pike96 <linyucheng256@live.com>
2017-10-19 02:27:15 -0700

    Realized trim

--- trimtest.py
+++ trimtest.py
@@ -1,30 +1,42 @@
 import wave
 import numpy as np
 import math
+import struct
 
 du = 0.5
 th = 0.02
+inputFName = 'test2.wav'
+outputFName = 'test2[Trimmed].wav'
 
-with wave.open('test2.wav') as fInput:
+# Read
+with wave.open(inputFName) as fInput:
     params = fInput.getparams()
-    nchannels, sampwidth, framerate, nframes = params[:4]
+    nchannels, sampwidth, framerate, nframes, comptype, compname = params[:6]
     strData = fInput.readframes(nframes)
     waveData = np.fromstring(strData, dtype=np.int16)
     waveData = waveData * 1.0 / (max(abs(waveData)))
     waveData = np.reshape(waveData, [nframes, nchannels])
 
+# Trim
 outData = waveData
 lenWindow = int(du * framerate)
 numWindow = math.floor(nframes / lenWindow)
 count = 0
+end = nframes
 for i in range(0, numWindow + 1):
     tempCh1 = waveData[i * lenWindow:min(lenWindow * (i + 1) - 1, nframes), 0]
     max_value = max(tempCh1)
     if max_value > th:
-        count = count + 1
+        count += 1
         start = (count - 1) * lenWindow
         end = start + len(tempCh1)
         outData[start:end, 0] = tempCh1
         outData[start:end, 1] = waveData[i * lenWindow:min(lenWindow * (i + 1) - 1, nframes), 1]
 outData = np.resize(outData,(end,2))
-print(1)
+
+# Save
+outData = np.reshape(outData,[end * nchannels, 1])
+with wave.open(outputFName, 'wb') as outwave:
+    outwave.setparams((nchannels, sampwidth, framerate, end, comptype, compname))
+    for v in outData:
+        outwave.writeframes(struct.pack('h', int(v * 64000 / 2)))  # outData:16位，-32767~32767，注意不要溢出
