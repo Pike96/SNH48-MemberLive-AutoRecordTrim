4beb21f37b9a3d77acd4b4c8a2ebd8d6cf51bdb6
Pike96 <linyucheng256@live.com>
2017-10-19 01:58:58 -0700

    Got output wave data

--- trimtest.py
+++ trimtest.py
@@ -1,16 +1,38 @@
 import wave
 import numpy as np
+import math
 
 du = 0.5
 th = 0.02
 
-fInput = wave.open('刘崇恬的电台Oct16-08-38.wav')
-params = fInput.getparams()
-nchannels, sampwidth, framerate, nframes = params[:4]
-strData = fInput.readframes(nframes)
-waveData = np.fromstring(strData,dtype=np.int16)
-waveData = waveData*1.0/(max(abs(waveData)))
-N = nframes
-time_duration = du
-time_length = time_duration*framerate
+with wave.open('test2.wav') as fInput:
+    params = fInput.getparams()
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    strData = fInput.readframes(nframes)
+    waveData = np.fromstring(strData, dtype=np.int16)
+    waveData = waveData * 1.0 / (max(abs(waveData)))
+    waveData = np.reshape(waveData, [nframes, nchannels])
+
+outData = waveData
+window = int(du * framerate)
+num_times = math.floor(nframes / window)
+count = 0
+number = 0
+for i in range(0, num_times + 1):
+    start = i * window
+    end = min(window * (i + 1) - 1, nframes)
+    time = waveData[i * window:min(window * (i + 1) - 1, nframes), 0]
+    #data = waveData[:, 0]
+    #arr = range(i * time_length, min(time_length * (i + 1) - 1, nframes))
+    #time = np.take(waveData[:,0], arr)
+    max_value = max(time)
+    if max_value > th:
+        count = count + 1
+        start = (count - 1) * window
+        end = start + len(time)
+        outData[start:end, 0] = time
+        outData[start:end, 1] = waveData[i * window:min(window * (i + 1) - 1, nframes), 1]
+x = outData[:end, 0]
+x2 = outData[:end, 1]
+outData = np.resize(outData,(end,2))
 print(1)
